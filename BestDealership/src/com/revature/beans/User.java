9f3e00775d5e596662ffda652ef7f5a1f1c39689
Adam Kager <atksource>
2019-06-25 07:54:45 -0400

    adding to basic structure

---
+++ BestDealership/src/com/revature/beans/User.java
@@ -0,0 +1,11 @@
+package com.revature.beans;
+
+public class User {
+	//this is a general user and is extended by customer and employee
+	private String userName = "TestName";
+	private String password = "TestPassword";
+	private String firstName = "Card";
+	private String lastName = "Holder";
+	private int logInStatus = 0;
+	
+}
---
+++ BestDealership/src/com/revature/beans/UserEmployee.java
@@ -0,0 +1,6 @@
+package com.revature.beans;
+
+public class UserEmployee {
+	//employees have a higher access level than customers
+	private final int accessLevel = 1;
+}
